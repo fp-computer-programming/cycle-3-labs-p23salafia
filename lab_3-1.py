91390ef690eeabe7e0a2ac549c210d049ff830dc
Jan <p23jsalafia@fairfieldprep.org>
2022-09-27 10:55:36 -0400

    3-1 Lab Submission

---
+++ lab_3-1.py
@@ -0,0 +1,6 @@
+magic_charge=0
+shield_charge=0
+if (magic_charge < 90) and (shield_charge < 75):
+    print("The dragon burns you to a crisp.")
+else:
+    print("You defeated the dragon! But the princess is in another castle.")
