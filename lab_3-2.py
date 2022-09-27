ed975c9831144b8e7f0d11c9b821ba1550ed3939
Jan <p23jsalafia@fairfieldprep.org>
2022-09-27 10:29:44 -0400

    3-2 Lab Submission

---
+++ lab_3-2.py
@@ -0,0 +1,10 @@
+int(a)
+#a is not defined
+int('a')
+#'a' is not an integer, and it is a string, not a variable
+'a'+2
+#a is not defined. Additionally, a is a string, not a variable
+import date
+#There is nothing to import named "date"
+print("I'm a happy camper)
+#Missing a "" at the end which closes the string
