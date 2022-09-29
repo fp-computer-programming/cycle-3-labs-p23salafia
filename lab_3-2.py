11483da39af27fd8bbfd78ea5b13236f21cf79c4
Jan <p23jsalafia@fairfieldprep.org>
2022-09-29 12:17:34 -0400

    3-2 Lab Final Submission

--- lab_3-2.py
+++ lab_3-2.py
@@ -1,10 +1,5 @@
-int(a)
-#a is not defined
-int('a')
-#'a' is not an integer, and it is a string, not a variable
-'a'+2
-#a is not defined. Additionally, a is a string, not a variable
-import date
-#There is nothing to import named "date"
-print("I'm a happy camper)
-#Missing a "" at the end which closes the string
+int(a)                      #a is not defined
+int('a')                    #'a' is not an integer, and it is a string, not a variable
+'a'+2                       #a is not defined. Additionally, a is a string, not a variable
+import date                 #There is nothing to import named "date"
+print("I'm a happy camper") #Missing a "" at the end which closes the string
