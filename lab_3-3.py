902f2814a8c72f679d26c777e3c5f61ff4d59786
Jan <p23jsalafia@fairfieldprep.org>
2022-09-29 12:17:58 -0400

    3-3 and 3-4 Lab Submission

---
+++ lab_3-3.py
@@ -0,0 +1,8 @@
+from random import randint, random, seed
+import random
+from random import randint, randrange
+
+print(randint(1,100)) #The numbers are different every time
+
+print(seed(5))
+print(randint(1,100)) #The number comes out the same every time
---
+++ lab_3.4.py
@@ -0,0 +1,22 @@
+import math
+var_1= math.ceil(7.15)          #Smallest Integer >=x is 8
+var_2= math.floor(19.156)       #Largest Integer <=x is 19
+var_3= math.fabs(-42)           #Absolute Value of x = 42
+var_4= math.pow (2,3)           #x^y = 2^3=8
+var_5= math.log10(100)          #Log base 10 of x = 2  or 10^2 = 100
+var_6= math.sqrt(9)             #Square root of 9 is 3
+var_7= math.isqrt(2)            #Integer square root of x = 1
+var_8= math.factorial(5)        #x! = 5*4*3*2*1
+var_9= math.gcd(111111111,12321)#Greatest Common denominator of the two numbers is 333 (333,667) and (37)
+var_10= math.sin(0)             #Sine of x = 0
+
+print(var_1)
+print(var_2)
+print(var_3)
+print(var_4)
+print(var_5)
+print(var_6)
+print(var_7)
+print(var_8)
+print(var_9)
+print(var_10)
