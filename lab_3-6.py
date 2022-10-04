885823cba2dd086ee9e0dc76ce068ab46de957e0
Jan <p23jsalafia@fairfieldprep.org>
2022-10-04 08:43:13 -0400

    3-6 Lab Final Submission

---
+++ lab_3-6.py
@@ -0,0 +1,13 @@
+var_string = 'Fairfield'
+var_two = 'Prep'
+print(var_string +' ' + var_two)
+
+var_line = '-'
+print(var_line * 20)
+
+var_statement_one = "I'm loving this short story I'm reading," 
+var_statement_two = '"The Fall of the House of Usher"'
+print(var_statement_one + var_statement_two)
+
+var_apple = 'Apple'
+print("l" in var_apple)
